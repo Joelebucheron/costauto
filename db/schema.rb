e868e2a638ef684f9729909709e381217176a963
Jeremy Blondeau <lebucheronjoe@gmail.com>
2022-05-17 15:02:24 +0200

    Users with nickname

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_11_195912) do
+ActiveRecord::Schema.define(version: 2022_05_17_121129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "brand"
+    t.string "model"
+    t.string "name"
+    t.integer "kilometers"
+    t.float "purchased_price"
+    t.date "purchased_date"
+    t.float "total_cost"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "garages", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "invoices", force: :cascade do |t|
+    t.string "name"
+    t.string "supplier"
+    t.string "piece"
+    t.float "price"
+    t.integer "kilometers"
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
