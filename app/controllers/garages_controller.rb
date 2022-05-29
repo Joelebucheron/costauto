e13f55c1d20754de982b6e05c4c91ac8ed7df9af
Jeremy Blondeau <lebucheronjoe@gmail.com>
2022-05-29 16:03:29 +0200

    ajout du controller et pages garages

---
+++ app/controllers/garages_controller.rb
@@ -0,0 +1,53 @@
+class GaragesController < ApplicationController
+
+  def index
+    @garages = Garage.all
+  end
+
+  def show
+    find
+  end
+
+  def new
+    @garage = Garage.new
+  end
+
+  def create
+    @garage = Garage.new(garage_params)
+    @garage.user = current_user
+    if @garage.save
+      redirect_to garage_path(@garage)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @garage = Garage.find(params[:id])
+  end
+
+  def update
+    find
+    if @garage.update(garage_params)
+      redirect_to garage_path(@garage)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    find
+    @garage.destroy
+    redirect_to garages_path
+  end
+
+  private
+
+  def garage_params
+    params.require(:garage).permit(:name)
+  end
+
+  def find
+    @garage = Garage.find(params[:id])
+  end
+end
