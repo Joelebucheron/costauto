fc8f1e3cce2f552b6a0a7f4705403310d16ac992
Jeremy Blondeau <lebucheronjoe@gmail.com>
2022-08-16 17:58:41 +0200

    adding kilometers to trips

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_04_195843) do
+ActiveRecord::Schema.define(version: 2022_08_16_154041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,9 @@ ActiveRecord::Schema.define(version: 2022_08_04_195843) do
     t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "kilometers"
+    t.integer "start_km"
+    t.integer "end_km"
     t.index ["car_id"], name: "index_trips_on_car_id"
   end
 
